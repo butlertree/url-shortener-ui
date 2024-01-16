c7c031bf25bb96a72afa8e8444f2550fc7ef1619
butlertree <140225441+butlertree@users.noreply.github.com>
2024-01-16 09:48:55 -0700

    added getrequest

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,19 +6,30 @@ import UrlForm from '../UrlForm/UrlForm';
 
 function App () {
   const [urls, setUrls] = useState([]);
+  const [error, setError] = useState('')
+  
 
-  useEffect(() => {
 
-  })
+useEffect(() => {
+  getUrls()
+    .then(data => { 
+    setUrls(data.urls)}) // Update the state 
+    .catch(error => setError(error.message));  //catch the error
+
+}, []);
+
+
+
 
   return (
     <main className="App">
       <header>
         <h1>URL Shortener</h1>
-        <UrlForm />
+        {/* <UrlForm addURL={addURL} /> */}
       </header>
 
-      <UrlContainer urls={"<<<Urls should go here>>>"}/>
+      <UrlContainer urls={urls}/>
+      {error && <h2 className='error-message'>Something went wrong.</h2>}
     </main>
   );
 }
